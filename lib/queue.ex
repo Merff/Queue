9910bac7dc9cece3493db57cd3ab87e1548cb2ee
Alex Mesh <merff.555@gmail.com>
2018-02-07 00:04:57 +0800

    add function for adding new messge to queue

--- lib/queue.ex
+++ lib/queue.ex
@@ -1,18 +1,34 @@
 defmodule Queue do
-  @moduledoc """
-  Documentation for Queue.
-  """
 
-  @doc """
-  Hello world.
+  alias Queue.Message
 
-  ## Examples
-
-      iex> Queue.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def add() do
+    {:error, "provide text message!"}
   end
+
+  def add(text)  do
+    case create_message(text) do
+      {:ok, _} ->
+        {:ok, "new message added!"}
+      {:error, changeset} ->
+        errors = humanaze_errors(changeset)
+        {:error, errors}
+    end
+  end
+
+
+  defp create_message(text) do
+    %Message{}
+    |> Message.changeset(%{text: text, priority: NaiveDateTime.utc_now()})
+    |> Queue.Repo.insert()
+  end
+
+  defp humanaze_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
+
 end
--- lib/queue/message.ex
+++ lib/queue/message.ex
@@ -6,4 +6,10 @@ defmodule Queue.Message do
     field :status, StatusEnum, default: 0
     field :priority, :utc_datetime
   end
+
+  def changeset(message, params \\ %{}) do
+    message
+    |> Ecto.Changeset.cast(params, [:text, :status, :priority])
+    |> Ecto.Changeset.validate_required([:text, :priority])
+  end
 end
